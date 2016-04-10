f133420e276e0c9608a36db453571ad77d65547e
WeslleyRei <midasgh6@gmail.com>
2016-04-09 21:47:40 -0300

    Create settings_load.php

---
+++ _modules/settings_load.php
@@ -0,0 +1,8 @@
+<?php
+	define('SESSION_NAME', 'DF5A279HCERUXCPA');
+
+	define('MYSQL_HOSTNAME', '127.0.0.1');
+	define('MYSQL_USERNAME', 'root');
+	define('MYSQL_PASSWORD', '');
+	define('MYSQL_DATABASE', 'General');
+?>
